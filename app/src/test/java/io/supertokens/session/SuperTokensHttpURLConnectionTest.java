bc631ae4c91f5145696d5305389fd53002aebebc
Nemi Shah <nemishah1212@gmail.com>
2019-09-10 16:33:39 +0530

    Add test cases for HttpURLConnection. [ci skip]

--- app/src/test/java/io/supertokens/session/SuperTokensHttpURLConnectionTest.java
+++ app/src/test/java/io/supertokens/session/SuperTokensHttpURLConnectionTest.java
@@ -2,12 +2,10 @@ package io.supertokens.session;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.text.TextUtils;
 import com.google.gson.Gson;
 import io.supertokens.session.android.MockSharedPrefs;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -20,10 +18,12 @@ import java.io.InputStreamReader;
 import java.net.CookieManager;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
-@SuppressWarnings("CatchMayIgnoreException")
+@SuppressWarnings({"CatchMayIgnoreException", "FieldCanBeLocal"})
 @RunWith(MockitoJUnitRunner.class)
 public class SuperTokensHttpURLConnectionTest {
     private final String testBaseURL = "http://192.168.29.145:8080/api/";
@@ -36,11 +36,9 @@ public class SuperTokensHttpURLConnectionTest {
 
     private final int sessionExpiryCode = 440;
     private static MockSharedPrefs mockSharedPrefs;
-    private static MockSharedPrefs.MockSharedPreferenceEditor mockSharedPrefsEditor;
 
     private static final String testIdRefreskPrefsKey = "st-test-id-refresh-prefs-key";
     private static final String testAntiCSRFPrefsKey = "st-test-anti-csrf-prefs-key";
-    private static final String TAG = SuperTokensHttpURLConnectionTest.class.getName();
 
     class UserInfoResponse {
         int counter;
@@ -51,11 +49,18 @@ public class SuperTokensHttpURLConnectionTest {
     @Mock
     Context context;
 
-    @BeforeClass
-    public static void beforeAll() {
+    @Before
+    public void beforeAll() {
         SuperTokens.isInitCalled = false;
         Mockito.mock(TextUtils.class);
         mockSharedPrefs = new MockSharedPrefs();
+        Mockito.when(application.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
+        Mockito.when(context.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
+        Mockito.when(application.getString(R.string.supertokensIdRefreshKey)).thenReturn(testIdRefreskPrefsKey);
+        Mockito.when(application.getString(R.string.supertokensAntiCSRFTokenKey)).thenReturn(testAntiCSRFPrefsKey);
+        Mockito.when(application.getString(R.string.supertokensSetCookieHeaderKey)).thenReturn("Set-Cookie");
+        Mockito.when(application.getString(R.string.supertokensAntiCSRFHeaderKey)).thenReturn("anti-csrf");
+        Mockito.when(application.getString(R.string.supertokensIdRefreshCookieKey)).thenReturn("sIdRefreshToken");
     }
 
     private void resetAccessTokenValidity(int lifetime) throws IOException {
@@ -82,7 +87,7 @@ public class SuperTokensHttpURLConnectionTest {
         StringBuilder builder = new StringBuilder();
         String line = reader.readLine();
         while (line != null) {
-            builder.append(line+"\n");
+            builder.append(line).append("\n");
             line = reader.readLine();
         }
 
@@ -128,13 +133,7 @@ public class SuperTokensHttpURLConnectionTest {
     @Test
     public void httpURLConnection_requestFailsIfCookieManagerNotSet() {
         boolean failed = true;
-        Mockito.when(application.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
-        Mockito.when(context.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
-        Mockito.when(application.getString(R.string.supertokensIdRefreshKey)).thenReturn(testIdRefreskPrefsKey);
-        Mockito.when(application.getString(R.string.supertokensAntiCSRFTokenKey)).thenReturn(testAntiCSRFPrefsKey);
-        Mockito.when(application.getString(R.string.supertokensSetCookieHeaderKey)).thenReturn("Set-Cookie");
-        Mockito.when(application.getString(R.string.supertokensIdRefreshCookieKey)).thenReturn("sIdRefreshToken");
-        Mockito.when(application.getString(R.string.supertokensAntiCSRFHeaderKey)).thenReturn("anti-csrf");
+        CookieManager.setDefault(null);
         try {
             SuperTokens.init(application, refreshTokenEndpoint, sessionExpiryCode);
             SuperTokensHttpURLConnection.newRequest(new URL(testAPiURL), new SuperTokensHttpURLConnection.SuperTokensHttpURLConnectionCallback<Integer>() {
@@ -160,13 +159,6 @@ public class SuperTokensHttpURLConnectionTest {
     @Test
     public void httpURLConnection_testAPIsWithoutParams() {
         boolean failed = false;
-        Mockito.when(application.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
-        Mockito.when(context.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
-        Mockito.when(application.getString(R.string.supertokensIdRefreshKey)).thenReturn(testIdRefreskPrefsKey);
-        Mockito.when(application.getString(R.string.supertokensAntiCSRFTokenKey)).thenReturn(testAntiCSRFPrefsKey);
-        Mockito.when(application.getString(R.string.supertokensSetCookieHeaderKey)).thenReturn("Set-Cookie");
-        Mockito.when(application.getString(R.string.supertokensIdRefreshCookieKey)).thenReturn("sIdRefreshToken");
-        Mockito.when(application.getString(R.string.supertokensAntiCSRFHeaderKey)).thenReturn("anti-csrf");
         try {
             resetAccessTokenValidity(10);
             SuperTokens.init(application, refreshTokenEndpoint, sessionExpiryCode);
@@ -192,13 +184,6 @@ public class SuperTokensHttpURLConnectionTest {
     @Test
     public void httpURLConnection_testAPIsWithParams() {
         boolean failed = false;
-        Mockito.when(application.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
-        Mockito.when(context.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
-        Mockito.when(application.getString(R.string.supertokensIdRefreshKey)).thenReturn(testIdRefreskPrefsKey);
-        Mockito.when(application.getString(R.string.supertokensAntiCSRFTokenKey)).thenReturn(testAntiCSRFPrefsKey);
-        Mockito.when(application.getString(R.string.supertokensSetCookieHeaderKey)).thenReturn("Set-Cookie");
-        Mockito.when(application.getString(R.string.supertokensIdRefreshCookieKey)).thenReturn("sIdRefreshToken");
-        Mockito.when(application.getString(R.string.supertokensAntiCSRFHeaderKey)).thenReturn("anti-csrf");
         try {
             resetAccessTokenValidity(10);
             SuperTokens.init(application, refreshTokenEndpoint, sessionExpiryCode);
@@ -220,17 +205,6 @@ public class SuperTokensHttpURLConnectionTest {
         }
 
         assertTrue(!failed);
-    }
-
-    @Before
-    public void beforeHttpURLConnection_testThatRefreshIsCalledAfterAccessTokenExpiry() {
-        Mockito.when(application.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
-        Mockito.when(context.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
-        Mockito.when(application.getString(R.string.supertokensIdRefreshKey)).thenReturn(testIdRefreskPrefsKey);
-        Mockito.when(application.getString(R.string.supertokensAntiCSRFTokenKey)).thenReturn(testAntiCSRFPrefsKey);
-        Mockito.when(application.getString(R.string.supertokensSetCookieHeaderKey)).thenReturn("Set-Cookie");
-        Mockito.when(application.getString(R.string.supertokensIdRefreshCookieKey)).thenReturn("sIdRefreshToken");
-        Mockito.when(application.getString(R.string.supertokensAntiCSRFHeaderKey)).thenReturn("anti-csrf");
     }
 
     @Test
@@ -277,17 +251,6 @@ public class SuperTokensHttpURLConnectionTest {
         assertTrue(!failed);
     }
 
-    @Before
-    public void beforeHttpURLConnection_refreshIsCalledIfAntiCSRFIsCleared() {
-        Mockito.when(application.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
-        Mockito.when(context.getSharedPreferences(Mockito.anyString(), Mockito.anyInt())).thenReturn(mockSharedPrefs);
-        Mockito.when(application.getString(R.string.supertokensIdRefreshKey)).thenReturn(testIdRefreskPrefsKey);
-        Mockito.when(application.getString(R.string.supertokensAntiCSRFTokenKey)).thenReturn(testAntiCSRFPrefsKey);
-        Mockito.when(application.getString(R.string.supertokensSetCookieHeaderKey)).thenReturn("Set-Cookie");
-        Mockito.when(application.getString(R.string.supertokensIdRefreshCookieKey)).thenReturn("sIdRefreshToken");
-        Mockito.when(application.getString(R.string.supertokensAntiCSRFHeaderKey)).thenReturn("anti-csrf");
-    }
-
     @Test
     public void httpURLConnection_refreshIsCalledIfAntiCSRFIsCleared() {
         boolean failed = false;
@@ -323,6 +286,75 @@ public class SuperTokensHttpURLConnectionTest {
 
             int refreshTokenCounter = getRefreshTokenCounter();
             if ( refreshTokenCounter != 1 ) {
+                throw new Exception("Refresh token counter value is not the same as the expected value");
+            }
+        } catch (Exception e) {
+            failed = true;
+        }
+
+        assertTrue(!failed);
+    }
+
+    @Test
+    public void httpURLConnection_refreshShouldBeCalledOnlyOnceForMultipleThreads() {
+        boolean failed = false;
+        try {
+            resetAccessTokenValidity(10);
+            SuperTokens.init(application, refreshTokenEndpoint, sessionExpiryCode);
+            CookieManager.setDefault(new CookieManager(new SuperTokensPersistentCookieStore(application), null));
+            int loginRequestCode = SuperTokensHttpURLConnection.newRequest(new URL(loginAPIURL), new SuperTokensHttpURLConnection.SuperTokensHttpURLConnectionCallback<Integer>() {
+                @Override
+                public Integer runOnConnection(HttpURLConnection con) throws IOException {
+                    con.setRequestMethod("POST");
+                    con.connect();
+                    return con.getResponseCode();
+                }
+            });
+            if ( loginRequestCode != 200 ) {
+                throw new Exception("Error making login request");
+            }
+
+            Thread.sleep(10000);
+            List<Runnable> runnables = new ArrayList<>();
+            final List<Boolean> runnableFailures = new ArrayList<>();
+            int runnableCount = 100;
+            for(int i = 0; i < runnableCount; i++) {
+                runnables.add(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            int userInfoResponseCode = SuperTokensHttpURLConnection.newRequest(new URL(userInfoAPIURL), new SuperTokensHttpURLConnection.SuperTokensHttpURLConnectionCallback<Integer>() {
+                                @Override
+                                public Integer runOnConnection(HttpURLConnection con) throws IOException {
+                                    con.setRequestMethod("GET");
+                                    con.connect();
+                                    return con.getResponseCode();
+                                }
+                            });
+                            if ( userInfoResponseCode != 200 ) {
+                                throw new Exception("Error connecting to userInfo");
+                            }
+
+
+                        } catch (Exception e) {
+                            runnableFailures.set(0, true);
+                        }
+                    }
+                });
+            }
+
+            for ( int i = 0; i < runnables.size(); i++ ) {
+                Runnable currentRunnable = runnables.get(i);
+                currentRunnable.run();
+            }
+
+            if ( !runnableFailures.isEmpty() ) {
+                // The only time we add to the list is when something fails
+                throw new Exception("One of the calls failed");
+            }
+
+            int refreshCount = getRefreshTokenCounter();
+            if ( refreshCount != 1 ) {
                 throw new Exception("Refresh token counter value is not the same as the expected value");
             }
         } catch (Exception e) {
