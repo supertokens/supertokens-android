bb9d7366f813470ffe4fedeaeb36795e23d2974f
Nemi Shah <nemishah1212@gmail.com>
2019-09-26 11:41:58 +0530

    Add unit tests for retrofit. [ci skip]

---
+++ app/src/test/java/io/supertokens/session/android/RetrofitTestAPIService.java
@@ -0,0 +1,23 @@
+package io.supertokens.session.android;
+
+import io.supertokens.session.SuperTokensRetrofitTest;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+public interface RetrofitTestAPIService {
+    @POST("login")
+    Call<Void> login();
+
+    @GET("userInfo")
+    Call<Void> userInfo();
+
+    @POST("testReset")
+    @Headers({
+        "Content-Type: application/json",
+        "Accept: application/json"
+    })
+    Call<Void> reset(@Header("atValidity") int validity);
+
+    @GET("testRefreshCounter")
+    Call<SuperTokensRetrofitTest.GetRefreshCounterResponse> refreshCounter();
+}
