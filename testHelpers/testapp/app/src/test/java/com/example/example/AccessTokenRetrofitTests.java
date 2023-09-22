aaf5452af92892d43c4e8365fbc84e0005a16f1a
Nemi Shah <nemishah1212@gmail.com>
2023-09-22 14:03:37 +0530

    Fix tests (#62)

--- testHelpers/testapp/app/src/test/java/com/example/example/AccessTokenRetrofitTests.java
+++ testHelpers/testapp/app/src/test/java/com/example/example/AccessTokenRetrofitTests.java
@@ -128,7 +128,22 @@ public class AccessTokenRetrofitTests {
                     "antiCsrfToken",
                     "iss",
                     "tId"
-            };
+                };
+            }
+
+            if (payload.has("rsub")) {
+                expectedKeys = new String[]{
+                        "sub",
+                        "exp",
+                        "iat",
+                        "sessionHandle",
+                        "refreshTokenHash1",
+                        "parentRefreshTokenHash1",
+                        "antiCsrfToken",
+                        "iss",
+                        "tId",
+                        "rsub"
+                };
             }
 
             assert payload.length() == expectedKeys.length;
@@ -192,6 +207,21 @@ public class AccessTokenRetrofitTests {
                         "antiCsrfToken",
                         "asdf",
                         "tId"
+                };
+            }
+
+            if (v3Payload.has("rsub")) {
+                expectedKeys = new String[]{
+                        "sub",
+                        "exp",
+                        "iat",
+                        "sessionHandle",
+                        "refreshTokenHash1",
+                        "parentRefreshTokenHash1",
+                        "antiCsrfToken",
+                        "asdf",
+                        "tId",
+                        "rsub"
                 };
             }
 
